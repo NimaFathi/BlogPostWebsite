b66564c56f9275aacc4848e374dd49792c9971a2
nima <nimafathinimafathi@gmail.com>
2019-07-26 03:33:07 +0430

    changed blog/views.py

--- website/blog/views.py
+++ website/blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import BlogPost
 # Create your views here.
 
-def blog_post_detail_page(request,id):
-    obj = get_object_or_404(BlogPost, id=id)
+def blog_post_detail_page(request,slug):
+    # obj = BlogPost.objects.get(slug=slug)
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name='blog_post_detail.html'
     context={'object':obj}
     return render(request, template_name, context)
