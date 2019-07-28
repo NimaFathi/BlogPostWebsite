ef2c16072d0f09df108dc56e8be3235c745253d0
nima <nimafathinimafathi@gmail.com>
2019-07-28 15:36:15 +0430

    change blog/views.py

--- website/blog/views.py
+++ website/blog/views.py
@@ -23,6 +23,7 @@ def blog_post_detail_page(request,slug):
 
 def blog_post_list_view(request):
     queryset = BlogPost.objects.all() # list of python objects
+    # as search: queryset = BlogPost.objects.filter(title__icontaints='something in here')
     template_name = 'blog_post_list.html'
     context = {'object_list':queryset}
     return render(request, template_name, context= context)
