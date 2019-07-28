10a890a0d6b75a5b171b0c8a3e7513c60d704a8f
nima <nimafathinimafathi@gmail.com>
2019-07-29 00:21:36 +0430

    make BlogPost subset of user

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
 
-class BlogPost(models.Model):
+#how to get users: from django.contrib.auth import get_user_model
+# j = User.onjects.first : (first user)
+# to take all BlogPosts: j.blogpost_set.all()
+
+class BlogPost(models.Model): #blogpost_set -> queryset
     # id = models.IntegerField() or pk
+    user = models.ForeignKey(User, default=1, null=True,on_delete=models.SET_NULL)
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique= True) # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
